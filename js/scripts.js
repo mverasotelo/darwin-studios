7a7ac3310e16b9bb58c9ce46ad915dbd0aed0ff1
mverasotelo <63819921+mverasotelo@users.noreply.github.com>
2020-10-22 21:58:30 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,18 @@
-const formSus = document.getElementById("suscripcion");
-if(formSus!=null){
-    formSus.addEventListener("submit", susConfirmacion);
-}
+const menuIcon = document.querySelector("#menuIcon");
+const menu = document.querySelector(".menu");
+const header = document.querySelector("header");
 
-function susConfirmacion(event){
-    event.preventDefault();
-    formSus.innerHTML=formSus.innerHTML+"<div class='confirma'>La suscripción se ha realizado correctamente!</div>";
-}
 
-const formRes = document.getElementById("reservar");
-if(formRes!=null){
-    formRes.addEventListener("submit", resConfirmacion);
-}
+menuIcon.addEventListener("click", despMenu);
 
-function resConfirmacion(event){
-    event.preventDefault();
-    let nombre= document.getElementById("name");
-    formRes.innerHTML=formRes.innerHTML+"<div class='confirma'>Reserva confirmada. Te esperamos, "+nombre.value+"!</div>";
-}
-
-const agregar = document.querySelectorAll(".order");
-
-if(agregar!=null){
-    for(i=0;i<agregar.length;i++){
-        agregar[i].addEventListener("click", agregarProducto);
-    }
-
-    function agregarProducto(event){
-        let cantidad=event.target.previousElementSibling.value;
-        let precio=event.target.parentNode.children[2].innerHTML;
-        if(cantidad>0&&cantidad<=10){
-            console.log("Agregaste "+cantidad+" "+event.target.value+" ( $"+Number(precio.substr(1))*cantidad+" )");
-        }
-        else if(cantidad>10)
-            console.log("Puedes ingresar hasta 10 unidades en un mismo pedido");
-        else{
-            console.log("Debes ingresar al menos una unidad");
-        }
+function despMenu(){
+    if(menuIcon.classList=="icon fas fa-bars"){
+        menuIcon.classList="icon fas fa-times";
+        menu.style.display="block";
+        header.style.background="rgba(0,0,0,0.5)";
+    }else{
+        menuIcon.classList="icon fas fa-bars";
+        menu.style.display="none";
+        header.style.background="transparent";
     }
 }
